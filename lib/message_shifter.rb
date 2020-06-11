42363a13729065c173ee7ce9b37d5f240e71c6b6
madking55 <madking55@gmail.com>
2020-06-11 21:58:49 +0200

    Add MessageShifter#char_shift

--- lib/message_shifter.rb
+++ lib/message_shifter.rb
@@ -6,23 +6,6 @@ class MessageShifter
     @shift = shift
   end
 
-  def shift_character(char, idx)
-    new_char = nil
-    if CHARS_SET.include?(char)
-      char_idx_in_message = idx
-      char_shift = @shift[0] if idx % 4 == 0
-      char_shift = @shift[1] if idx % 4 == 1
-      char_shift = @shift[2] if idx % 4 == 2
-      char_shift = @shift[3] if idx % 4 == 3
-      char_idx_in_charset = CHARS_SET.index(char)
-      new_idx = char_idx_in_charset + char_shift
-      new_char = CHARS_SET[new_idx % 27]
-    else
-      new_char = char
-    end
-    new_char
-  end
-
   def shift_message
     shifted_message = ''
     @message.downcase.each_char.with_index do |char, idx|
@@ -32,14 +15,32 @@ class MessageShifter
     shifted_message
   end
 
+  def unshift_message
+    unshifted_message = '' 
+    @message.each_char.with_index do |char, idx|
+     new_char = unshift_character(char, idx)
+     unshifted_message += new_char
+    end
+    unshifted_message
+  end
+
+  def shift_character(char, idx)
+    new_char = nil
+    if CHARS_SET.include?(char)
+      char_shift = char_shift(idx)
+      char_idx_in_charset = CHARS_SET.index(char)
+      new_idx = char_idx_in_charset + char_shift
+      new_char = CHARS_SET[new_idx % 27]
+    else
+      new_char = char
+    end
+    new_char
+  end
+
   def unshift_character(char, idx)
     new_char = nil
     if CHARS_SET.include?(char)
-      char_idx_in_message = idx
-      char_shift = @shift[0] if idx % 4 == 0
-      char_shift = @shift[1] if idx % 4 == 1
-      char_shift = @shift[2] if idx % 4 == 2
-      char_shift = @shift[3] if idx % 4 == 3
+      char_shift = char_shift(idx)
       char_idx_in_charset = CHARS_SET.index(char)
       new_idx = char_idx_in_charset - char_shift
       new_char = CHARS_SET[new_idx % 27]
@@ -49,12 +50,11 @@ class MessageShifter
     new_char
   end
 
-  def unshift_message
-   unshifted_message = '' 
-   @message.each_char.with_index do |char, idx|
-    new_char = unshift_character(char, idx)
-    unshifted_message += new_char
-   end
-   unshifted_message
+  def char_shift(idx)
+    return @shift[0] if idx % 4 == 0
+    return @shift[1] if idx % 4 == 1
+    return @shift[2] if idx % 4 == 2
+    return @shift[3] if idx % 4 == 3
   end
+
 end
